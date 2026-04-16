f13ea56efc0bf108c67afe1b50e2492f81f1d4ba
Joao Pedro <joaopedro2007.pessoal@gmail.com>
2026-04-16 16:29:10 -0500

    leitorV1.4.6

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 from flask import render_template, request, flash
 from werkzeug.utils import secure_filename
-from services.importador_medicao import extrair_medicao_consolidada
+from importador_medicao import extrair_medicao
 from flask import redirect
 
 from services.importador_medicao import extrair_medicao
